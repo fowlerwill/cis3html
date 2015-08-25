b6298be3b04b95d391f4244625ef25547081d7c5
Coulton Fraser <cjrfraser@gmail.com>
2015-08-25 16:56:27 -0600

    Added includes php files

---
+++ includes/call2action.php
@@ -0,0 +1,30 @@
+<!-- Call 2 Action Image & Buttons -->
+
+<div class="header">
+	<ul class="subnav row hide-for-small">
+		<li class=""><a href="#">What is CIS3?</a></li>
+		<li class=""><a href="#">New Students!</a></li>
+	</ul>
+	<section id="about-cutandfill" class=" hide-for-large hide-for-medium row small-10 medium-8 large-6">
+		<!--Spacing Hack for buttons im too lazy-->
+		<?php 
+			<div class="row">
+				<p></p>
+			</div>
+			<div class="row">
+				<p></p>
+			</div>
+
+
+			<!--Spacing Hack-->
+			<div class="small-12 medium-6 large-6 columns text-center">
+				<a class="button expand action-button">What is CIS3?</a>
+			</div>
+
+			<div class="small-12 medium-6 large-6 columns text-center">
+				<a class="button expand action-button">New Students!</a>
+			</div>
+
+	</section>
+</div>
+<!-- /end of call 2 action section -->

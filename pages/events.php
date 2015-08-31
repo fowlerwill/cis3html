5420dc338f2d9d087d09e16fc906a27eacdcdc87
Coulton Fraser <cjrfraser@gmail.com>
2015-08-31 00:48:07 -0600

    Added CIS3 Logo to events page, and fixed the top nav bar height so the menu options are no longer cut down.

--- pages/events.php
+++ pages/events.php
@@ -5,15 +5,26 @@
  * Date: 28/08/15
  */
 ?>
+	<div class="logo-body highlight ">
+		<div class="row full-width dark logo-container">
+			<div class="small-offset-3 small-6 large-offset-3 large-6 text-center title-line">
+				<img class="thumbnail-height" src="" data-interchange="[img/CIS-v2.png, (default)], [img/CIS-v2.png, (large)]">
+				<noscript><img src="img/CIS-v2.png">
+				</noscript>
 
-<div class="row">
-    <div class="column small-12 padding-top-double">
-        <h1>CIS3 Events:</h1>
-    </div>
-</div>
+			</div>
 
-<div class="row">
-    <div class="column small-12 padding padding-top-double text-center light">
-        <iframe id="eventCalendar" src="https://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23D9D9D9&amp;src=mtroyal.ca_t35263g1j9995rc0mojd17eufo%40group.calendar.google.com&amp;color=%232952A3&amp;ctz=America%2FEdmonton" style=" border-width:0 " width="100%" frameborder="0" scrolling="no"></iframe>
-    </div>
-</div>
+			<div class="row">
+
+				<div class="text-center">
+					<h1>Events</h1>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="row">
+		<div class="column small-12 padding padding-top-double text-center light">
+			<iframe id="eventCalendar" src="https://www.google.com/calendar/embed?showTitle=0&amp;showPrint=0&amp;showCalendars=0&amp;showTz=0&amp;height=600&amp;wkst=1&amp;bgcolor=%23D9D9D9&amp;src=mtroyal.ca_t35263g1j9995rc0mojd17eufo%40group.calendar.google.com&amp;color=%232952A3&amp;ctz=America%2FEdmonton" style=" border-width:0 " width="100%" frameborder="0" scrolling="no"></iframe>
+		</div>
+	</div>
--- pages/home.php
+++ pages/home.php
@@ -3,7 +3,7 @@
 -->
 <div class="highlight logo-body">
 <div class="logo-container full-width dark">
-<div class="row title-">
+<div class="row">
     <div class="text-center large-8 columns large-offset-2 small-8 small-offset-2 title-line">
         <a href="index.html"><img class="logo-max-height" src="" data-interchange="[img/CIS-v2.png, (default)], [img/CIS-v2.png, (large)]">
             <noscript><img src="img/CIS-v2.png">

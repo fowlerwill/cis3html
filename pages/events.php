f5df2ace461e47c60a60e214b76ce61cd3db0893
Coulton Fraser <cjrfraser@gmail.com>
2015-09-06 13:25:12 -0600

    Adding executive photos

--- pages/events.php
+++ pages/events.php
@@ -6,7 +6,7 @@
  */
 ?>
 	<div class="logo-body highlight ">
-		<div class="row full-width dark logo-container">
+		<div class="row full-width dark">
 			<div class="small-offset-3 small-6 large-offset-3 large-6 text-center title-line">
 				<img class="thumbnail-height" src="" data-interchange="[img/CIS-v2.png, (default)], [img/CIS-v2.png, (large)]">
 				<noscript><img src="img/CIS-v2.png">

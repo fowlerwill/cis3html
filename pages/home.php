be919cff9a15388cd0026992580527e6aa492532
Coulton Fraser <cjrfraser@gmail.com>
2015-09-30 23:56:32 -0600

    Changed Home page and new area

--- pages/home.php
+++ pages/home.php
@@ -2,45 +2,47 @@
     LOGO
 -->
 <div class="highlight logo-body">
-<div class="logo-container full-width dark">
-    <div class="row">
-        <div class="text-center large-8 columns large-offset-2 small-8 small-offset-2 title-line">
-            <a href="index.html"><img class="logo-max-height" src="home" data-interchange="[img/CIS-v2.png, (default)], [img/CIS-v2.png, (large)]">
-                <noscript><img src="img/CIS-v2.png">
-                </noscript>
-            </a>
-        </div>
-    </div>
+	<div class="logo-container full-width dark">
+		<div class="row">
+			<div class="text-center large-8 columns large-offset-2 small-8 small-offset-2 title-line">
+				<a href="index.html"><img class="logo-max-height" src="home" data-interchange="[img/CIS-v2.png, (default)], [img/CIS-v2.png, (large)]">
+					<noscript><img src="img/CIS-v2.png">
+					</noscript>
+				</a>
+			</div>
+		</div>
 
-    <div class="row ">
-        <div class="columns text-center small-8 small-offset-2">
-            <h1 class="link-animation cis-title white-text">Computer Information Systems Student Society</h1>
-        </div>
-    </div>
-</div>
+		<div class="row ">
+			<div class="columns text-center small-8 small-offset-2">
+				<h1 class="link-animation cis-title white-text">Computer Information Systems Student Society</h1>
+			</div>
+		</div>
 	</div>
+</div>
 
 <!--
     FULL WIDTH IMAGE
 -->
-<div id="introImage" class="hide-for-small row fullWidth margin-top-double"></div>
+<!--<div id="introImage" class="hide-for-small row fullWidth margin-top-double"></div>-->
 
 <!--
     CALL TO ACTION BUTTONS
 -->
+<!--
 <div class="row padding-top-double">
-    <div class="medium-4 columns text-center">
-        <a class="button expand action-button" href="forEmployers">For Employers</a>
-    </div>
+	<div class="medium-4 columns text-center">
+		<a class="button expand action-button" href="forEmployers">For Employers</a>
+	</div>
 
-    <div class="medium-4 columns text-center">
-        <a class="button expand action-button" href="events">See our events</a>
-    </div>
+	<div class="medium-4 columns text-center">
+		<a class="button expand action-button" href="events">See our events</a>
+	</div>
 
-    <div class="medium-4 columns text-center">
-        <a class="button expand action-button" href="forStudents">For Students</a>
-    </div>
+	<div class="medium-4 columns text-center">
+		<a class="button expand action-button" href="forStudents">For Students</a>
+	</div>
 </div>
+-->
 
 <!--	Main Content Area	  -->
 
@@ -49,40 +51,43 @@
 		<p>CIS3 is a club for the students of Mount Royal University's Bachelor of Computer Information Systems and Computer Science transfer programs. CIS3 aims to complete the student experience by providing what school alone cannot. Kinship, esprit de corps, the social component of success, provided by the friends that gather together to discuss relevant issues around their degrees, and their industry, as well as relaxing, playing video games, board games, networking with peers and industry.</p>
 		<p>If you a new to the club be sure to check out the executive page to connect with the team, and see projects our executives are working on. And to those returning for another year, you can expect bigger and better events.</p>
 	</div>
-    <div class="row">
-        <div class="column">
-            <h2>News</h2>
-            <hr>
-        </div>
-    </div>
-    <!--  NEWS ROLL  -->
-    <?php
+	<div class="row">
+		<div class="column">
+			<h2>Announcements</h2>
+			<hr>
+		</div>
+	</div>
+	<!--  NEWS ROLL  -->
+	<?php
         //TODO: BUILD A BLOG ROLL OF THE MOST RECENT 3 news ENTRIES
     ?>
-    <div class="row padding-top">
-        <div class="column medium-4 text-right">
-            <a href="http://www.fallcon.com">
-                <img src="img/news/fallcon.jpg" class="img-responive" alt="Fallcon poster">
-            </a>
-        </div>
-        <div class="column medium-8">
-            <h3>Fallcon is coming up! Get your tickets</h3>
-            <p>Come and play boardgames at FallCon 28, October 16 through 18. Immense game library, friendly game teachers, tournaments, panels, massive auction, and massiver flea market. Find out more at
-                <a href="http://www.fallcon.com" target="_blank" class="font-black underline-black">www.fallcon.com</a></p>
-        </div>
-    </div>
+		<div class="row padding-top">
+			<ul class="small-block-grid-1 medium-block-grid-2" data-equalizer>
 
-    <div class="row padding-top">
-        <div class="column medium-8">
-            <h3>The yearly Hitachi ID Systems puzzle!</h3>
-            <p>These guys always put together a great puzzle, and this year is no exception.</p>
-            <p>Go and see them at the science and engineering career fair at the Jack Simpson Gymnasium at U of C on September 22nd 10am - 4pm</p>
-        </div>
-        <div class="column medium-4 text-right">
-            <a href="http://www.hitachi-id.com">
-                <img src="img/news/hitachipuzzle.png" class="img-responive" alt="Hitachi ID Systems Hiring Puzzle">
-            </a>
-        </div>
-    </div>
-
+				<li class="text-center">
+					<div class="margin" data-equalizer-watch>
+						<a class="th" href="http://www.fallcon.com">
+							<img src="img/news/fallcon.jpg" width="150px">
+						</a>
+					</div>
+					<div class="floor">
+						<h3>Fallcon is coming! Get your tickets.</h3>
+						<p>Come and play boardgames at FallCon 28, October 16 through 18. Immense game library, friendly game teachers, tournaments, panels, massive auction, and massiver flea market. Find out more at
+							<a href="http://www.fallcon.com" target="_blank" class="font-black underline-black">www.fallcon.com</a></p>
+					</div>
+				</li>
+				<li class="text-center" >
+					<div class="margin" data-equalizer-watch>
+						<a class="th" href="img/news/hitachipuzzle.png">
+							<img src="img/news/hitachipuzzle.png" width="200px">
+						</a>
+					</div>
+					<div class="floor">
+						<h3>The yearly Hitachi ID Systems puzzle!</h3>
+						<p>These guys always put together a great puzzle, and this year is no exception.</p>
+						<p>Go and see them at the science and engineering career fair at the Jack Simpson Gymnasium at U of C on September 22nd 10am - 4pm</p>
+					</div>
+				</li>
+			</ul>
+		</div>
 </div>

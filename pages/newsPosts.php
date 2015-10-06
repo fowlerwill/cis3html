f4a31892902d0e958087c8954d616e73ad18d40a
Coulton Fraser <cjrfraser@gmail.com>
2015-10-06 17:34:56 -0600

    Added newsPosts.php and newsPosts.json files

---
+++ pages/newsPosts.php
@@ -0,0 +1,18 @@
+<div class="">
+
+</div>
+
+<!--
+3 Posts at a time with a pagination for every other post.
+Search Function to find posts with specific tags.
+
+Each post will contain info from json file:
+TITLE:
+TEXT:
+IMG:
+
+!GET so pages can be bookmarked
+
+
+
+-->

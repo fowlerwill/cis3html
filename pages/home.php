c8a3325cf591100b1039114fceb6add7ea45ea72
Will <wfowler@madebyuppercut.com>
2015-10-21 19:48:53 -0600

    added sci fi party info

--- pages/home.php
+++ pages/home.php
@@ -66,12 +66,34 @@
 
 				<li class="text-center">
 					<div class="margin" data-equalizer-watch>
+						<a class="th" href="https://www.paypal.com/cgi-bin/webscr?cmd=_xclick&business=fowlerwill%40gmail.com&lc=CA&item_name=CIS3+Sci+Fi+Party+Ticket&amount=11.00&currency_code=CAD&button_subtype=services&shipping=0.00&bn=PP-BuyNowBF%3Abtn_buynowCC_LG.gif%3ANonHosted">
+							<img src="img/news/scifiparty.jpg" width="150px">
+						</a>
+					</div>
+					<div class="floor">
+						<h3>Get your tickets to the Sci Fi Costume party!</h3>
+						<p>CIS3 has partnered up with the CBSS and Improvnation clubs to bring you this awesome party at
+                        the Hub! <strong>Thursday, October 29th at 8pm</strong> wear your favourity Sci Fi costume! Buy
+                        your tickets online and pick them up at the door, or buy them with cash from Will in the lab</p>
+                        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                            <input type="hidden" name="cmd" value="_s-xclick">
+                            <input type="hidden" name="encrypted" value="-----BEGIN PKCS7-----MIIHdwYJKoZIhvcNAQcEoIIHaDCCB2QCAQExggEwMIIBLAIBADCBlDCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb20CAQAwDQYJKoZIhvcNAQEBBQAEgYBLUgu42mSOBjirjUZO4f/8Cn5BY1Ap7v3dAvJ6qctFe3ZVC+iCbAPQ2UrH93gI0tHGROLUSfKu5v1lqTF11n82QTBLHPFfacI1ZTy2T8W6YT+x6hNApYWvT0WSHyV8b9vlbBIEPfqd9nLzdF+eVIBkps0VCVRbL0/OZ1b9Aw6JFzELMAkGBSsOAwIaBQAwgfQGCSqGSIb3DQEHATAUBggqhkiG9w0DBwQI8bx+pND/XYSAgdAgyoXgWj6tEJpQZANVAXzjS4gGOumU2uJLVFpfj9gTYr9FxEUx9MwapqFWHkU4uRAPxQdYUDkYEGI2fU+QYf3FKw3iOYr0/lfOaAf6GfwKKTCwQ91C5vcYAaDHXykrlsxxRLQbm81QTGnrzoZNCOpJvCHKhk/JgRvKJJMs9f/U+Xxf1g2XDhD9lInSztPRUSnR5HuRh9qwOoCT5suO6v6Rrk89ExKm0iniOA8p3axJh2yyLB2NpCIDEaAciM3P1SmtiC23l5ddulmoCVxygi+qoIIDhzCCA4MwggLsoAMCAQICAQAwDQYJKoZIhvcNAQEFBQAwgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMB4XDTA0MDIxMzEwMTMxNVoXDTM1MDIxMzEwMTMxNVowgY4xCzAJBgNVBAYTAlVTMQswCQYDVQQIEwJDQTEWMBQGA1UEBxMNTW91bnRhaW4gVmlldzEUMBIGA1UEChMLUGF5UGFsIEluYy4xEzARBgNVBAsUCmxpdmVfY2VydHMxETAPBgNVBAMUCGxpdmVfYXBpMRwwGgYJKoZIhvcNAQkBFg1yZUBwYXlwYWwuY29tMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDBR07d/ETMS1ycjtkpkvjXZe9k+6CieLuLsPumsJ7QC1odNz3sJiCbs2wC0nLE0uLGaEtXynIgRqIddYCHx88pb5HTXv4SZeuv0Rqq4+axW9PLAAATU8w04qqjaSXgbGLP3NmohqM6bV9kZZwZLR/klDaQGo1u9uDb9lr4Yn+rBQIDAQABo4HuMIHrMB0GA1UdDgQWBBSWn3y7xm8XvVk/UtcKG+wQ1mSUazCBuwYDVR0jBIGzMIGwgBSWn3y7xm8XvVk/UtcKG+wQ1mSUa6GBlKSBkTCBjjELMAkGA1UEBhMCVVMxCzAJBgNVBAgTAkNBMRYwFAYDVQQHEw1Nb3VudGFpbiBWaWV3MRQwEgYDVQQKEwtQYXlQYWwgSW5jLjETMBEGA1UECxQKbGl2ZV9jZXJ0czERMA8GA1UEAxQIbGl2ZV9hcGkxHDAaBgkqhkiG9w0BCQEWDXJlQHBheXBhbC5jb22CAQAwDAYDVR0TBAUwAwEB/zANBgkqhkiG9w0BAQUFAAOBgQCBXzpWmoBa5e9fo6ujionW1hUhPkOBakTr3YCDjbYfvJEiv/2P+IobhOGJr85+XHhN0v4gUkEDI8r2/rNk1m0GA8HKddvTjyGw/XqXa+LSTlDYkqI8OwR8GEYj4efEtcRpRYBxV8KxAW93YDWzFGvruKnnLbDAF6VR5w/cCMn5hzGCAZowggGWAgEBMIGUMIGOMQswCQYDVQQGEwJVUzELMAkGA1UECBMCQ0ExFjAUBgNVBAcTDU1vdW50YWluIFZpZXcxFDASBgNVBAoTC1BheVBhbCBJbmMuMRMwEQYDVQQLFApsaXZlX2NlcnRzMREwDwYDVQQDFAhsaXZlX2FwaTEcMBoGCSqGSIb3DQEJARYNcmVAcGF5cGFsLmNvbQIBADAJBgUrDgMCGgUAoF0wGAYJKoZIhvcNAQkDMQsGCSqGSIb3DQEHATAcBgkqhkiG9w0BCQUxDxcNMTUxMDIyMDE0NzAzWjAjBgkqhkiG9w0BCQQxFgQUZtLhlcf3w4KAKpfWQXRDBO7hZF0wDQYJKoZIhvcNAQEBBQAEgYC5XULUskqlW7lHS9AjnOhCn0xRh4vFZ0gpiVIrL86EMPBi2VkrzUYAgCni2wWbh/8zhAjIZqHn2D5Jd1grN7CLCNR0Y7gmB02BgE5khsXM9mE6s7ubRBzKLRkpHEWdk2xEQ37l3SJ/ClaXlroz7jx1W5JJcHGMP+UK/adhFxPqPA==-----END PKCS7-----
+">
+                            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_buynowCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                        </form>
+
+                    </div>
+				</li>
+
+				<li class="text-center">
+					<div class="margin" data-equalizer-watch>
 						<a class="th" href="http://www.fallcon.com">
 							<img src="img/news/fallcon.jpg" width="150px">
 						</a>
 					</div>
 					<div class="floor">
-						<h3>Fallcon is coming! Get your tickets.</h3>
+						<h3>Fallcon Board Games</h3>
 						<p>Come and play boardgames at FallCon 28, October 16 through 18. Immense game library, friendly game teachers, tournaments, panels, massive auction, and massiver flea market. Find out more at
 							<a href="http://www.fallcon.com" target="_blank" class="font-black underline-black">www.fallcon.com</a></p>
 					</div>
